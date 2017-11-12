09f0700596bc693ce5053dc2dbf4e0c6a74d486a
Ez <ezrabl42@gmail.com>
2017-11-12 03:38:42 -0500

    Adds text areas

---
+++ src/server/make_db.js
@@ -0,0 +1,53 @@
+//Make the database
+
+var mysql = require('mysql');
+var db_name = 'noteDatabase'; 
+
+
+//Make the credentials
+var db = mysql.createConnection(
+        {
+            host: 'localhost',
+            user: 'root',
+            password: 'IAMGROOT',
+            multipleStatements: true 
+        });
+
+//Upon connection
+db.connect(function(err)
+        {
+            if(err)
+            {
+                console.log(err);
+            }
+            else
+            {
+                console.log('Connected');
+                //clean up
+                db.query('DROP DATABASE IF EXISTS ' + db_name + '; CREATE DATABASE '+ db_name +'; USE ' + db_name +';',
+                        function (err, result)
+                        {
+                            if(err)
+                            {
+                                console.log(this.sql);
+                                console.log(err);
+                            }
+                            else console.log('Database clean.');
+                        });
+
+                //the tables
+                db.query('CREATE TABLE snippets (user VARCHAR(20), entrytime DATETIME, score DECIMAL, note TEXT);'
+                        +'CREATE TABLE masterdoc (id TINYINT, user VARCHAR(20), entrytime DATETIME, note TEXT)',
+                        function(err, result)
+                        {
+                            if(err)
+                            {
+                                console.log(this.sql);
+                                console.log(err);
+                            }
+                            else console.log('Tables created.');   
+                        });
+            }
+            
+            db.end();
+        });
--- src/server/serverbundle.js
+++ src/server/serverbundle.js
@@ -16869,10 +16869,10 @@ app.use(express.static(path.join(__dirname, '../client/public')));
 
 //Database credentials
 var db = mysql.createConnection(
-        {
+        { 
             host: 'localhost',
             user: 'root',
-            database: 'test',
+            database: 'noteDatabase',
             password: 'IAMGROOT'
         });
 
@@ -16880,21 +16880,26 @@ var db = mysql.createConnection(
 db.connect(function(err)
         {
             if(err) console.log(err);
+            else
+            {
+                console.log('Database connection created.');
+            }
         });
 
 //Definition of global vars
 var numUsers = 0;
-var notes = [];
-var isInitNotes = false;
+var snippets = [];
+var approved = [];
+var isInit = false;
 var socketCount = 0;
-
+var MySqlDateTime = function() {return (new Date ((new Date((new Date(new Date())).toISOString() )).getTime() - ((new Date()).getTimezoneOffset()*60000))).toISOString().slice(0, 19).replace('T', ' ')};
 
 
 //adding a connection
 io.on('connection', function(socket)
         {
-            var addedUser = false;
             socketCount++;
+            //push to all sockets the number of users
             io.sockets.emit('users connected', socketCount);
 
             //disconnect
@@ -16903,37 +16908,74 @@ io.on('connection', function(socket)
                         socketCount--;
                         io.sockets.emit('users connected', socketCount);
                     });
-
+            
+            
+            
             //check initial query
-            if(!isInitNotes)
+            if(!isInit)
             {
                 //Initial start
-                db.query('SELECT * FROM notes')
+             
+
+                db.query('SELECT * FROM snippets')
                     .on('result', function(data)
                         {
                             //push results
-                            notes.push(data);
+                            snippets.push(data);
                         })
                     .on('end', function()
                         {
                             //emit after finished
-                            socket.emit('initial notes', notes)
+                            socket.emit('initial snippets', snippets)
                         });
-                isInitNotes = true;
+                isInit = true;
             }
             else
             {
                 //inital notes exist
-                socket.emit('initial notes', notes)
+                socket.emit('initial snippets', snippets);
             };
 
-           //new note added
-            socket.on('new note', function(data)
+
+
+
+            //new note added
+            socket.on('new snippet', function(data)
                     {
-                        notes.push(data);
-                        io.sockets.emit('new note', data);
-                        //db injection
-                        db.query('INSERT INTO notes (note) VALUES (?)', data.note);
+                        snippets.push(data);
+                        //push to sockets
+                        io.sockets.emit('new snippet', data);
+                        //db injection --how to generalize?
+                        var now = MySqlDateTime();
+                        db.query('INSERT INTO snippets (user, entrytime, score, note) VALUES (?,?,?,?);', [data.user, now, 0, data.note],
+                                function(err, results)
+                                {
+                                    if(err) 
+                                    {
+                                        console.log(this.sql);
+                                        console.log(err);
+                                    }
+                                });
+
+                        var contrib = 
+                        {
+                            "id"    :   approved.length,
+                            "user"  :   data.user,
+                            "entrytime":now,
+                            "note"  :   data.note
+                        };
+
+                        approved.push(contrib);
+                        
+                        db.query('INSERT INTO masterdoc (id, user, entrytime, note) VALUES (?,?,?,?);', [approved.length, data.user, now, data.note],
+                                function(err, results)
+                                {
+                                    if(err)
+                                    {
+                                        console.log(this.sql);
+                                        console.log(err);
+                                    }
+                                });
                     });
         });                            
 
